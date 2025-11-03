8a801ff9369457e57d7747172f8ef8553bcc812d
Sarah <sarah.l04@aluno.ifsc.edu.br>
2025-11-03 20:51:02 -0300

    Adição arquivos

---
+++ Aula1-Calculadora/backup/unit1.pas
@@ -0,0 +1,54 @@
+unit Unit1;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Panel4: TPanel;
+    pnFundo: TPanel;
+    pnDisplay: TPanel;
+    Panel3: TPanel;
+    pnFundoBotoes: TPanel;
+    procedure FormCreate(Sender: TObject);
+    procedure Panel3Click(Sender: TObject);
+    procedure pnDisplayClick(Sender: TObject);
+  private
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.Panel3Click(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.pnDisplayClick(Sender: TObject);
+begin
+
+end;
+
+end.
+
