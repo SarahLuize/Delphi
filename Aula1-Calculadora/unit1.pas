a3a956e920589060315a14b94e5ed2497c1e70f6
Sarah <sarah.l04@aluno.ifsc.edu.br>
2025-11-09 14:07:47 -0300

    Adicionado botões

--- Aula1-Calculadora/unit1.pas
+++ Aula1-Calculadora/unit1.pas
@@ -5,21 +5,48 @@ unit Unit1;
 interface
 
 uses
-  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls;
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, ExtCtrls, StdCtrls;
 
 type
 
   { TForm1 }
 
   TForm1 = class(TForm)
+    btn7: TButton;
+    btn4: TButton;
+    btn5: TButton;
+    btn6: TButton;
+    btn8: TButton;
+    btn9: TButton;
+    btn0: TButton;
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
     Panel4: TPanel;
+    pnLinha1: TPanel;
     pnFundo: TPanel;
     pnDisplay: TPanel;
-    Panel3: TPanel;
+    pnFundoBotoesNumericos: TPanel;
     pnFundoBotoes: TPanel;
+    pnLinha2: TPanel;
+    pnLinha3: TPanel;
+    pnLinha4: TPanel;
+    procedure Button10Click(Sender: TObject);
+    procedure Button11Click(Sender: TObject);
+    procedure Button12Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Button2Click(Sender: TObject);
+    procedure Button3Click(Sender: TObject);
+    procedure Button4Click(Sender: TObject);
+    procedure Button7Click(Sender: TObject);
+    procedure Button8Click(Sender: TObject);
+    procedure Button9Click(Sender: TObject);
     procedure FormCreate(Sender: TObject);
-    procedure Panel3Click(Sender: TObject);
+    procedure onClickButton2(Sender: TObject);
+    procedure Panel4Click(Sender: TObject);
     procedure pnDisplayClick(Sender: TObject);
+    procedure pnFundoClick(Sender: TObject);
+    procedure pnLinha1Click(Sender: TObject);
   private
 
   public
@@ -35,25 +62,23 @@ implementation
 
 { TForm1 }
 
-procedure TForm1.FormCreate(Sender: TObject);
+procedure TForm1.Panel4Click(Sender: TObject);
 begin
-
-end;
-
-procedure TForm1.Panel3Click(Sender: TObject);
-begin
-
+  Panel4.BorderSpacing.Left := 0;
 end;
 
 procedure TForm1.pnDisplayClick(Sender: TObject);
 begin
-  Panel4.BorderSpacing.Left := 0;
-  ShowMessage(
-  'Top: ' + IntToStr(Panel4.BorderSpacing.Top) + sLineBreak +
-  'Bottom: ' + IntToStr(Panel4.BorderSpacing.Bottom) + sLineBreak +
-  'Left: ' + IntToStr(Panel4.BorderSpacing.Left) + sLineBreak +
-  'Right: ' + IntToStr(Panel4.BorderSpacing.Right)
-);
+
+end;
+
+procedure TForm1.pnFundoClick(Sender: TObject);
+begin
+
+end;
+
+procedure TForm1.pnLinha1Click(Sender: TObject);
+begin
 
 end;
 
